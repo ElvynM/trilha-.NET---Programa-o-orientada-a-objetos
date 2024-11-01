22302f03353f949f3ae39ee5fa6d266c7faa9b38
ElvynM <elvynmartins30@gmail.com>
2024-11-01 20:37:48 -0300

    subindo projeto em orientação objeto c#

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+ Smartphone nokia = new Nokia("123456789", "Nokia X20", "1111111111", 64);
+ nokia.Ligar();
+ nokia.ReceberLigacao();
+ nokia.InstalarAplicativo("WhatsApp");
+ Console.WriteLine();
+
+ Smartphone iphone = new Iphone("987654321", "iPhone 13", "2222222222", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+Console.ReadLine();
